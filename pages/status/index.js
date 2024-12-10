deb39141294d10e6fe4c82ab0f972c1d24bbbfe9
luiscarlosjp <jardimpereiralc@gmail.com>
2024-12-10 16:41:33 -0300

    feat: add status page

---
+++ pages/status/index.js
@@ -0,0 +1,28 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+  let updated_at = "Carregando ...";
+
+  if (!isLoading && data) {
+    updated_at = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+  return <div>Última atualização: {updated_at}</div>;
+}
